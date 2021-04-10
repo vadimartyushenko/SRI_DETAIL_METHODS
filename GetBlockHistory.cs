7f4acb2f57ff4e17f195fce22494a1dcda0e7d25
Artyushenko Vadim <artushenkomail@mail.ru>
2021-04-10 15:18:30 +0700

    Изменен алгоритм
    Изменен алгоритм формирования промежутков блокировки

--- GetBlockHistory.cs
+++ GetBlockHistory.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-
 namespace DatumNode
 {
-
   public class CfsBlockItem
   {
     public string type_block { get; set; }
@@ -14,7 +12,8 @@ namespace DatumNode
     public DateTime? date_end { get; set; }
     public string orderId_begin { get; set; }
     public string orderId_end { get; set; }
-    public DateTime cfs_audit_date { get; set; }
+    public string id_begin { get; set; }
+  //  public DateTime cfs_audit_date { get; set; }
   }
   public class AuditItem
   {
@@ -27,8 +26,10 @@ namespace DatumNode
     public string public_document_group_id { get; set; }
     public DateTime cfs_audit_date { get; set; }
   }
+
   public static class Script
   {
+    public const string fin = "Финансовая";
     public static void Run(DatumNodeService datumnode, string cfs_id, out System.Collections.Generic.List<DatumNode.CfsBlockItem> result)
     {
       if (cfs_id == null)
@@ -58,108 +59,82 @@ namespace DatumNode
         }).ToList();
 
         var auditListFiltered = auditList.Where(x => x.GetType().GetProperties().Where(p => p.PropertyType == typeof(DateTime?)).Select(p => p.GetValue(x)).Any(value => value != null));
-             
+        if (!auditListFiltered.Any())
+          return;
+        
         var auditBlockArr = auditListFiltered.ToArray();
 
         var finBlockList = new List<DatumNode.CfsBlockItem>();
         var admBlockList = new List<DatumNode.CfsBlockItem>();
         var t = new CfsBlockItem();
 
+        
         for (int i = auditBlockArr.Length-1; i > 0; i--)
 				{
           if (auditBlockArr[i].cfs_fin_block_begin.HasValue == false && auditBlockArr[i-1].cfs_fin_block_begin.HasValue == false)
             continue;
-          if (auditBlockArr[i].cfs_fin_block_begin.HasValue == false && auditBlockArr[i-1].cfs_fin_block_begin.HasValue == true)
-          {
-            t = new CfsBlockItem() {
-              type_block = "fin",
-              date_begin = auditBlockArr[i-1].cfs_fin_block_begin.Value,
+          if (auditBlockArr[i].cfs_fin_block_begin.HasValue == false && auditBlockArr[i - 1].cfs_fin_block_begin.HasValue == true ||
+              auditBlockArr[i].cfs_fin_block_begin.Value != auditBlockArr[i-1].cfs_fin_block_begin.Value)
+            t = new CfsBlockItem()
+            {
+              type_block = fin,
+              date_begin = auditBlockArr[i - 1].cfs_fin_block_begin.Value,
               date_end = null,
               orderId_begin = auditBlockArr[i-1].public_document_group_id,
-              cfs_audit_date = auditBlockArr[i-1].cfs_audit_date
+              orderId_end = null,
+              id_begin = auditBlockArr[i - 1].public_document_group_id
             };
-            //finBlockList.Add(t);
+          if (auditBlockArr[i].cfs_fin_block_begin.HasValue == true && auditBlockArr[i].cfs_fin_block_end.HasValue == false && auditBlockArr[i - 1].cfs_fin_block_end.HasValue == true)
+          {
+            t.date_end = auditBlockArr[i - 1].cfs_fin_block_end;
+            t.orderId_end = auditBlockArr[i - 1].public_document_group_id;
+            finBlockList.Add(t);
             continue;
           }
-          if (auditBlockArr[i].cfs_fin_block_begin.Value == auditBlockArr[i-1].cfs_fin_block_begin.Value)
-          {
-            if (auditBlockArr[i].cfs_fin_block_end.HasValue == false && auditBlockArr[i-1].cfs_fin_block_end.HasValue == true)
-            {
-              var t = new CfsBlockItem()
-              {
-                type_block = "fin",
-                date_begin = null,
-                date_end = auditBlockArr[i-1].cfs_fin_block_end,
-                 orderId_begin = auditBlockArr[i-1].public_document_group_id,
-                cfs_audit_date = auditBlockArr[i-1].cfs_audit_date
-              };
-              finBlockList.Add(t);
-            }
-          }
-          else
-          {
-              var t = new CfsBlockItem()
-              {
-                type_block = "fin",
-                date_begin = auditBlockArr[i-1].cfs_fin_block_begin,
-                date_end = null,
-                 orderId_begin = auditBlockArr[i-1].public_document_group_id,
-                cfs_audit_date = auditBlockArr[i-1].cfs_audit_date
-              };
-            finBlockList.Add(t);
-          }
 				};
+        if (t.date_begin.HasValue == true && t.date_end.HasValue == false)
+        {
+          finBlockList.Add(t);
+          t = new CfsBlockItem();
+        }
 
         for (int i = auditBlockArr.Length - 1; i > 0; i--)
         {
           if (auditBlockArr[i].cfs_block_begin.HasValue == false && auditBlockArr[i - 1].cfs_block_begin.HasValue == false)
             continue;
-          if (auditBlockArr[i].cfs_block_begin.HasValue == false && auditBlockArr[i - 1].cfs_block_begin.HasValue == true)
-          {
-            var t = new CfsBlockItem()
+          if (auditBlockArr[i].cfs_block_begin.HasValue == false && auditBlockArr[i - 1].cfs_block_begin.HasValue == true ||
+              auditBlockArr[i].cfs_block_begin.Value != auditBlockArr[i - 1].cfs_block_begin.Value)
+            t = new CfsBlockItem()
             {
               type_block = "adm",
               date_begin = auditBlockArr[i - 1].cfs_block_begin.Value,
               date_end = null,
-               orderId_begin = auditBlockArr[i - 1].public_document_group_id,
-              cfs_audit_date = auditBlockArr[i - 1].cfs_audit_date
+              orderId_begin = auditBlockArr[i - 1].public_document_group_id,
+              orderId_end = null
+              //cfs_audit_date = auditBlockArr[i - 1].cfs_audit_date
             };
+          if (auditBlockArr[i].cfs_block_begin.HasValue == true && auditBlockArr[i].cfs_block_end.HasValue == false && auditBlockArr[i - 1].cfs_block_end.HasValue == true)
+          {
+            t.date_end = auditBlockArr[i - 1].cfs_block_end;
+            t.orderId_end = auditBlockArr[i - 1].public_document_group_id;
             admBlockList.Add(t);
             continue;
           }
-          if (auditBlockArr[i].cfs_block_begin.Value == auditBlockArr[i - 1].cfs_block_begin.Value)
-          {
-            if (auditBlockArr[i].cfs_block_end.HasValue == false && auditBlockArr[i - 1].cfs_block_end.HasValue == true)
-            {
-              var t = new CfsBlockItem()
-              {
-                type_block = "fin",
-                date_begin = null,
-                date_end = auditBlockArr[i - 1].cfs_block_end,
-                 orderId_begin = auditBlockArr[i - 1].public_document_group_id,
-                cfs_audit_date = auditBlockArr[i - 1].cfs_audit_date
-              };
-              admBlockList.Add(t);
-            }
-          }
-          else
-          {
-            var t = new CfsBlockItem()
-            {
-              type_block = "fin",
-              date_begin = auditBlockArr[i-1].cfs_block_begin,
-              date_end = null,
-               orderId_begin = auditBlockArr[i - 1].public_document_group_id,
-              cfs_audit_date = auditBlockArr[i - 1].cfs_audit_date
-            };
-            admBlockList.Add(t);
-          }
         };
+        if (t.date_begin.HasValue == true && t.date_end.HasValue == false)
+        {
+          admBlockList.Add(t);
+        }
 
         var resultSet = new List<DatumNode.CfsBlockItem>();
-        resultSet.AddRange(finBlockList);
+        
+        if(finBlockList.Any())
+         resultSet.AddRange(finBlockList);
 
-        result = resultSet.OrderByDescending(x => x.cfs_audit_date).ToList();
+        if(admBlockList.Any())
+         resultSet.AddRange(admBlockList);
+
+        result = resultSet.Any() ? resultSet.OrderByDescending(x => x.date_begin).ThenByDescending(x => x.date_end).ToList(): new List<CfsBlockItem>();
       }
       catch (Exception e)
       {
